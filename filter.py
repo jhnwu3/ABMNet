340a9b1834dca38eb7076abebf083966273de89a
John Wu <jhnwu3@gmail.com>
2023-02-05 15:07:15 -0500

    $

---
+++ filter.py
@@ -0,0 +1,49 @@
+import pandas as pd 
+import numpy as np
+
+
+file = 'data/NL6P.csv'
+nParams = 6
+nOutputs = 27
+df = pd.read_csv(file)
+data = df.to_numpy()
+indices_changing = np.array([0,2,5])
+const_val = 0.4
+initial_moments = np.loadtxt('data/time_series/initial_6pro_moments.txt',delimiter=',') 
+print(initial_moments)
+# exit(0)
+to_save = []
+
+
+
+# For when you want input == output 33 to 27
+nIn = nParams + nOutputs
+for r in range(data.shape[0]):
+    vec = np.zeros(nIn+nOutputs)
+    vec[:nParams] = data[r,:nParams]
+    vec[nParams:nIn] = initial_moments
+    vec[nIn:nIn+nOutputs] = data[r,nParams:]
+    to_save.append(vec)
+
+# For when you wanna limit the data sizes.
+# for row in range(data.shape[0]):
+#     save_row = True
+#     for c in range(nParams):
+#         if c not in indices_changing:
+#             # print(c)
+#             save_row = save_row and data[row,c] == const_val 
+#     if save_row:
+#         to_save.append(data[row,:])
+
+to_save = np.array(to_save)
+
+
+cols = []
+for i in range(nIn):
+    cols.append('k' + str(i + 1))
+
+for o in range(nOutputs):
+    cols.append('o' + str(o+1))
+print(to_save.shape)
+new_df = pd.DataFrame(to_save, columns=cols)
+new_df.to_csv('data/NL6_in.csv')
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 python3 main.py -i 'data/linear.csv' -h 100 --epochs 100 -d 2 -o 'linear3protein'
 python3 main.py -i 'data/gdag_400ss.csv' -h 1000 --epochs 1000 -d 3 -o 'gdagABM_more_avg'
 python3 main.py -i 'data/NL6P.csv' -h 1000 --epochs 1000 -d 5 -o 'nl6'
+python3 main.py -i 'data/NL6S.csv' -h 64 --epochs 100 -d 5 -o 'nl6s'
 python3 main.py -i 'data/gdag_1300ss.csv' -h 100 --epochs 1000 -d 5 -o 'gdag1300' --normalize
 python3 main.py -i 'data/gdag_1300ss_covs.csv' -h 64 --epochs 100 -d 5 -o 'gdag1300_covs' --normalize
 python3 main.py -i 'data/gdag_1300ss.csv' -h 100 --epochs 100 -d 5 -o 'gdag1300_res_net' --gpu --normalize
+
+python3 main.py -i 'data/NL6_in.csv' -h 64 --epochs 100 -d 5 -o 'nl6in'
