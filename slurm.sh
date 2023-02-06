4639dfe334d7989aa852ed0a950fee972630ed5a
jhnwu3 <rsjxw007@r1pl-hpcf-log01.crii.org>
2023-02-06 08:58:34 -0500

    $

--- slurm.sh
+++ slurm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#SBATCH --time=0-10:10:00 
+#SBATCH --time=0-20:10:00 
 #SBATCH --job-name=ABMNet_GPU
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --output=./data/slurm_outputs/gdagn_res%j.txt
+#SBATCH --output=./data/slurm_outputs/nl6_res_means%j.txt
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:a100
 
 #python3 main.py -i 'data/NL6P.csv' --epochs 110 -h 128 -d 6 -o 'nl6_normalize_large' --gpu --normalize
 
-python3 main.py -i 'data/gdag_1300ss.csv' -h 128 --epochs 120 -d 32 -o 'gdag1300ss_large_norm' --gpu --normalize --type res_nn
+#python3 main.py -i 'data/gdag_1300ss.csv' -h 128 --epochs 200 -d 32 -o 'gdag1300ss_large_norm' --gpu --normalize --type res_nn
+
+#python3 main.py -i 'data/NL6_in.csv' -h 64 -d 10 -o 'nl6in' --epochs 200 --gpu
+
+python3 main.py -i 'data/NL6_means.csv' -h 512 -d 32 -o 'nl6means_large' --epochs 1000 --gpu --type res_nn --normalize
