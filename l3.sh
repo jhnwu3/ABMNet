c8acf311f2838078fd209abbed393364064a1f0b
jhnwu3 <rsjxw007@r1pl-hpcf-log01.crii.org>
2023-03-17 18:51:30 -0400

    $

--- l3.sh
+++ l3.sh
@@ -10,10 +10,10 @@
 # python3 main.py -i 'data/static/l3p_t1.csv' -d 4 -h 64 --epochs 50 -o 'l3p_t1' --save --gpu
 # python3 main.py -i 'data/static/l3p_t2.csv' -d 4 -h 64 --epochs 50 -o 'l3p_t2' --save --gpu
 # python3 main.py -i 'data/static/l3p_t3.csv' -d 4 -h 64 --epochs 50 -o 'l3p_t3' --save --gpu
-python3 main.py -i 'data/time_series/l3pt_i.csv' -d 4 -h 64 --epochs 50 -o 'l3p_i' --save --gpu
+#python3 main.py -i 'data/time_series/l3pt_i.csv' -d 4 -h 64 --epochs 50 -o 'l3p_i' --save --gpu
 # python3 main.py -i 'data/static/l3p_10k_t3_5kss.csv' -d 6 -h 128 --epochs 50 -o 'l3p_10k_5kss' --save --gpu
 
-# python3 main.py -i 'data/static/l3p_10k_t3_5kss.csv' -d 4 -h 64 --epochs 200 -o 'l3p_100k_small_res_t3' --save --gpu --type res_nn --normalize --normalize_out
+python3 main.py -i 'data/static/l3p_10k_t3_5kss.csv' -d 4 -h 64 --epochs 100 -o 'l3p_100k_small_res_t3' --save --gpu --type res_nn --normalize --normalize_out
 # python3 main.py -i 'data/static/l3p_100k.csv' -d 6 -h 64 --epochs 50 -o 'l3p_100k_small' --save --gpu 
 #python3 main.py -i 'data/NL6_means.csv' -h 128 -d 5 -o 'nl6means_h128_d5.csv' --epochs 100 --normalize --type res_n --gpu
 
