8c69e031a8bb8ed760a7dbe57f2775b7da0eee29
John Wu <jhnwu3@gmail.com>
2023-05-10 19:12:52 -0400

    stuff

--- data/spatial.py
+++ data/spatial.py
@@ -7,8 +7,8 @@ from pathlib import Path
 from textwrap import wrap
 import sys
 import os
-
-
+import pickle
+import torch as tc
 
 
 # each spatial object has 3 specific components
@@ -42,8 +42,8 @@ class SpatialObj():
                 dump_i = SpatialObj.process_dump_file(file_i)
                 dump_t = SpatialObj.process_dump_file(file_t)
                 # translate
-                data_i = SpatialObj.translate_to_x_y(dump_i)
-                data_t = SpatialObj.translate_to_x_y(dump_t)
+                data_i = SpatialObj.translate_to_img(dump_i)
+                data_t = SpatialObj.translate_to_img(dump_t)
                 # append
                 data.append((data_i, data_t))
             
@@ -62,8 +62,8 @@ class SpatialObj():
         return data
         
     #Pixel, Cytotoxic CD8+ T Cells, Cancer, Exhausted CD8+ T Cells, Dead Cancer Cells, Ignore, Ignore, TAMs, Ignore
-    #  x,y,class
-    def translate_to_x_y(matrix, width=100, features=9):
+    #  matrix[x,y,feature]
+    def translate_to_img(matrix, width=100, features=9):
         # convert indices in far left column to 
         # get some tensor feature
         organized = np.zeros((width, width, features))
@@ -73,7 +73,6 @@ class SpatialObj():
             organized[row,column] = matrix[i,1:]
         
         return organized
-         
         
     def __init__(self, dirname : str):
         self.dir = dirname
@@ -82,11 +81,11 @@ class SpatialObj():
         self.n_samples = len(self.data)
         
 
-class GiuseppeSpatialDataLoader():
+class GiuseppeSpatialDataProcessor():
     def __init__(self, path, single_initial_cond = True):
         # each spatial object contains a set of parameters 
         self.path = path
-        self.spatialData = GiuseppeSpatialDataLoader.get_spatial_objs(path)
+        self.spatialData = GiuseppeSpatialDataProcessor.get_spatial_objs(path)
         
     def get_spatial_objs(path : str, single_initial_cond = True):
         parameter_dirs = [os.path.join(path,x) for x in os.listdir(path)]
@@ -97,17 +96,67 @@ class GiuseppeSpatialDataLoader():
                 obj = SpatialObj(dir)
                 spatialObjs.append(obj)
         return spatialObjs    
-        
+    
+    # it will convert all of the objects in the data structure into a dictionary, and then save as a pickle file once done. 
+    # Probably should've been done this way originally we can keep things clean.
+    def convert_to_pickle(self, path="file.pickle"):
+        dictionary = {} # every key will be the set of rate constants and every value is a list of 30 tuples of (in, out)
+        for obj in self.spatialData:
+            dictionary[obj.rates.tobytes()] = obj.data
+        with open(path, 'wb') as handle:
+            pickle.dump(dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+# come back to this once we can create a pickle file/ dictionary for Pytorch Geometric
+class GiuseppeSurrogateGraphData():
+    def __init__(self, single_initial_cond = True):
+        self.input_graphs = [] # list of torch array of all features row by row, each row a node
+        self.output_graphs = [] # list of torch array with each row a node as well.
+        self.edges = [] # torch array of all edges 
+        self.rates = []# list of rates we care about in torch
+        self.single_init = single_initial_cond
+    
+    def delaunay_edges_and_data(self, dictionary):
+        for key in dictionary.keys():
+            rates = np.frombuffer(key)
+            self.rates.append(tc.from_numpy(rates))
+            lattice_shape = dictionary[key][0][0].shape
+            
+            coords = np.zeros((lattice_shape[0] * lattice_shape[1], 2))
+            i = 0
+            for r in range(sample[0].shape[0]):
+                for c in range(sample[1].shape[1]):
+                    # create array of 2D coordinates for triangulation
+                    coords[i,0] = r
+                    coords[i,1] = c
+                    i+=1
+            
+            # now get the edges from delaunay triangulation that will be reused across all things
+            
+            # 
+            for sample in dictionary[key]: 
+                print(sample)        
+        if self.single_init: # what if we only need one of the initial conditions
+            self.input_data = self.input_data[0]
+
+    # save to pickle
+    def save(self,path):
+        print("Saving Graph Data to PKL")
 
 parent_data_dir = "../../share/Giuseppe_John/training_data_with_dump_files_ParamSweep_11-03-2023_30_samples/"
+# parent_data_dir = "data/spatial/"
 
-
-test = GiuseppeSpatialDataLoader(parent_data_dir)
+test = GiuseppeSpatialDataProcessor(parent_data_dir)
 print(test.spatialData[0].rates)
 print(test.spatialData[1].rates)
-
 print(test.spatialData[0].data[0][0])
+test.convert_to_pickle("../gdag_test.pickle")
+# loadedDict = pickle.load(open("data/spatial/test.pickle","rb"))
+# for keys in loadedDict.keys():
+#     print(np.frombuffer(keys))
+
+# load one set of parameter rate constants in, 
+
+
 
 # Currently data looks something a little like this, 
 # parent_data_dir
---
+++ process_gdag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=0-40:10:00 
+#SBATCH --job-name=ABMNet_GPU
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --output=./data/slurm_outputs/nl6_res_means%j.txt
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:a100
+
+# python3 main.py -i 'data/static/NL6P.csv' --epochs 110 -h 128 -d 6 -o 'nl6_normalize_large' --gpu --normalize --normalize_out
+python3 data/spatial.py
+#python3 main.py -i 'data/NL6_means.csv' -h 128 -d 5 -o 'nl6means_h128_d5.csv' --epochs 100 --normalize --type res_n --gpu
+
+#python3 main.py -i 'data/gdag_1300ss_covs.csv' -h 128 --epochs 200 -d 20 -o 'gdag1300ss_large_norm' --gpu --normalize --type res_nn
+
+#python3 main.py -i 'data/NL6_in.csv' -h 64 -d 10 -o 'nl6in' --epochs 200 --gpu
+
+#python3 main.py -i 'data/NL6_means.csv' -h 512 -d 32 -o 'nl6means_large' --epochs 1000 --gpu --type res_nn --normalize
