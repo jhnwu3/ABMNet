cd9333109ddca69050e747b6d05672f39afa3fa3
John Wu <jhnwu3@gmail.com>
2023-05-16 13:11:42 -0400

    $

--- process_gdag.sh
+++ process_gdag.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu
 
 # python3 main.py -i 'data/static/NL6P.csv' --epochs 110 -h 128 -d 6 -o 'nl6_normalize_large' --gpu --normalize --normalize_out
-python3 data/spatial.py
+python3 spatial.py
 #python3 main.py -i 'data/NL6_means.csv' -h 128 -d 5 -o 'nl6means_h128_d5.csv' --epochs 100 --normalize --type res_n --gpu
 
 #python3 main.py -i 'data/gdag_1300ss_covs.csv' -h 128 --epochs 200 -d 20 -o 'gdag1300ss_large_norm' --gpu --normalize --type res_nn
