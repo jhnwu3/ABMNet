0d5981f2ab306423b7be955cbf7ec46eb32b548a
John Wu <jhnwu3@gmail.com>
2023-05-23 16:17:22 -0400

    $

--- process_gdag.sh
+++ process_gdag.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=0-2:10:00 
+#SBATCH --time=0-10:10:00 
 #SBATCH --job-name=SpatialABMNet
 #SBATCH --partition=general,das,himem
 #SBATCH --nodes=1
 #SBATCH --output=./data/slurm_outputs/spatial_data%j.txt
 #SBATCH --cpus-per-task=30
-#SBATCH --gres=gpu
+#SBATCH --gres=gpu:a100
 # python3 main.py -i 'data/static/NL6P.csv' --epochs 110 -h 128 -d 6 -o 'nl6_normalize_large' --gpu --normalize --normalize_out
 python3 spatial.py
 # mprof plot
