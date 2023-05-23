cd52a21e4cb8571da5554db69fe0b4a1915505a5
John Wu <jhnwu3@gmail.com>
2023-05-23 16:28:02 -0400

    $

--- process_gdag.sh
+++ process_gdag.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-#SBATCH --time=0-2:10:00 
+#SBATCH --time=0-10:10:00 
 #SBATCH --job-name=SpatialABMNet
 #SBATCH --partition=general,das,himem
 #SBATCH --nodes=1
 #SBATCH --output=./data/slurm_outputs/spatial_data%j.txt
 #SBATCH --cpus-per-task=30
-#SBATCH --gres=gpu
-# python3 main.py -i 'data/static/NL6P.csv' --epochs 110 -h 128 -d 6 -o 'nl6_normalize_large' --gpu --normalize --normalize_out
+#SBATCH --gres=gpu:a100
+
 python3 spatial.py
 # mprof plot
 # python3 -m memory_profiler spatial.py
-
-#python3 main.py -i 'data/NL6_means.csv' -h 128 -d 5 -o 'nl6means_h128_d5.csv' --epochs 100 --normalize --type res_n --gpu
-
-#python3 main.py -i 'data/gdag_1300ss_covs.csv' -h 128 --epochs 200 -d 20 -o 'gdag1300ss_large_norm' --gpu --normalize --type res_nn
-
-#python3 main.py -i 'data/NL6_in.csv' -h 64 -d 10 -o 'nl6in' --epochs 200 --gpu
-
-#python3 main.py -i 'data/NL6_means.csv' -h 512 -d 32 -o 'nl6means_large' --epochs 1000 --gpu --type res_nn --normalize
