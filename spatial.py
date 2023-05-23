70b800292af79d3a8d7724029c5a6fcaf8ca3080
John Wu <jhnwu3@gmail.com>
2023-05-23 14:53:26 -0400

    $

--- spatial.py
+++ spatial.py
@@ -3,23 +3,19 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
-from pathlib import Path
-from textwrap import wrap
-import sys
-import os
-import pickle
 import torch 
-import torch.nn.functional as F
-import torch.nn as nn
-import resource
-from torch_geometric.nn import GCNConv
-from scipy import spatial
 from modules.utils.graph import *
 from modules.data.spatial import *
 from modules.models.spatial import *
 
 data = SingleInitialConditionDataset("../gdag_data/gdag_graph_data.pickle")
-nEpochs = 5
+train_size = int(0.8 * len(data))
+test_size = len(data) - train_size
+
+# split dataset, into training set and test
+train_data, test_data = torch.utils.data.random_split(data, [train_size, test_size])
+
+nEpochs = 50
 # for manual testing, load everything at once, and train
 # model = GCN(n_features=input_graph.size()[1], n_classes=output_graphs_chunk[0].size()[1])
 model = GCNComplex(n_features=data.n_inputs, n_classes= data.n_outputs, n_rates=data.n_rates, hidden_channels=32)
@@ -38,7 +34,7 @@ else:
     device = torch.device("cpu")
     using_gpu = False
     
-dataloader = torch.utils.data.DataLoader(data, batch_size=None, shuffle=True) 
+dataloader = torch.utils.data.DataLoader(train_data, batch_size=None, shuffle=True) 
 for epoch in range(nEpochs):
     loss_per_epoch = 0
     for rates, output_graph in dataloader:
@@ -51,6 +47,16 @@ for epoch in range(nEpochs):
       
     if epoch % 1 == 0:
         print("Epoch:", epoch, " Loss:", loss_per_epoch)   
+
+
+test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=None, shuffle=True)
+model.eval()
+test_loss = 0
+for rates, output_graph in test_dataloader:
+    out = model(data.initial_graph.to(device), data.edges.to(device), rates.to(device))
+    test_loss += criterion(out, output_graph.to(device))
+
+print("Test Average MSE:", test_loss / len(test_data))
 
 
 # for i in range(len(output_graphs_chunk)):
