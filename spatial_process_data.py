db740edd6305ba8ecddc7a1eec106b01a3c9f701
John Wu <jhnwu3@gmail.com>
2023-05-26 12:32:29 -0400

    $

--- spatial_process_data.py
+++ spatial_process_data.py
@@ -10,8 +10,10 @@ from modules.data.spatial import *
 from modules.models.spatial import *
 
 path = "../gdag_data/gdag_test_full.pickle"
+print("Reading From:", path)
 lattice_data = pickle.load(open(path, "rb"))
 data_processed = GiuseppeSurrogateGraphData()
+print("Computing Delaunay Moments")
 data_processed.delaunay_moments(lattice_data, channels=[0,1,2,3,6])
 print(data_processed.output_graphs[0].size())
 print(len(data_processed.input_graphs))
