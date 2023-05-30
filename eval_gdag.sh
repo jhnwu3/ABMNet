0be98a260214ca0d39c37ebba38389c05d8292dc
John Wu <jhnwu3@gmail.com>
2023-05-30 09:43:51 -0400

    $

---
+++ eval_gdag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=0-10:10:00 
+#SBATCH --job-name=SpatialABMNetTraining
+#SBATCH --partition=general,das,himem
+#SBATCH --nodes=1
+#SBATCH --output=./data/slurm_outputs/spatial_eval%j.txt
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:a100
+
+python3 spatial_evaluate.py
+# mprof plot
+# python3 -m memory_profiler spatial.py
