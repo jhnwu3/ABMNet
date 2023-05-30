f03e74ff760f00920bc672317908edfba403bd3a
John Wu <jhnwu3@gmail.com>
2023-05-30 09:44:01 -0400

    $

--- eval_gdag.sh
+++ eval_gdag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=0-10:10:00 
-#SBATCH --job-name=SpatialABMNetTraining
+#SBATCH --job-name=SpatialABMNetEvaluation
 #SBATCH --partition=general,das,himem
 #SBATCH --nodes=1
 #SBATCH --output=./data/slurm_outputs/spatial_eval%j.txt
