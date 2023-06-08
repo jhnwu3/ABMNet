76bfa3590eb706428e5dfeef562d7fc7ef1b074e
John Wu <jhnwu3@gmail.com>
2023-06-08 10:37:00 -0400

    $

--- generate_indrani.sh
+++ generate_indrani.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --output=./data/slurm_outputs/temporal_generate%j.txt
 #SBATCH --cpus-per-task=2
-
-python3 data/John_Indrani_data/zeta/training_kon_koff/training_data_generation.py
+cd data/John_Indrani_data/zeta/training_kon_koff/
+python3 training_data_generation.py
 # mprof plot
 # python3 -m memory_profiler spatial.py
