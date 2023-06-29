07542694b0abad5a309bdecc2cbeb60eaebd123b
John Wu <jhnwu3@gmail.com>
2023-06-29 12:29:56 -0400

    $

--- slurm_scripts/playground.sh
+++ slurm_scripts/playground.sh
@@ -1,1 +1,6 @@
 python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t750.csv' -d 4 -h 64 --epochs 50 -o 'ixr_1k_zeta_ca_t750' --save --gpu --normalize
+
+
+git add .
+git commit -m "$"
+git push
