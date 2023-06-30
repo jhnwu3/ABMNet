920f8475274a19b0e78f4e1a837ac674fd145098
John Wu <jhnwu3@gmail.com>
2023-06-30 12:26:04 -0400

    $

--- modules/data/temporal.py
+++ modules/data/temporal.py
@@ -189,6 +189,8 @@ def generate_static_dataset(dataset : TemporalDataset, t):
 
 
 def combine_temporal_pickles(file1, file2, save=True, path=""):
+    data1 = None
+    data2 = None
     with open(file1, 'rb') as f1, open(file2, 'rb') as f2:
         data1 = pickle.load(f1)
         data2 = pickle.load(f2)
@@ -200,7 +202,7 @@ def combine_temporal_pickles(file1, file2, save=True, path=""):
     
     if save:
         with open(path, 'wb') as f:
-            pickle.dump(data, f)
+            pickle.dump(combined_data, f)
     
     return combined_data
 
