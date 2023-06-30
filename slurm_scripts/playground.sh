c3b47f6f79f202e1602268547783b246e963d309
John Wu <jhnwu3@gmail.com>
2023-06-30 16:00:52 -0400

    $

--- slurm_scripts/playground.sh
+++ slurm_scripts/playground.sh
@@ -1,9 +1,9 @@
 git pull
 
-python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t750.csv' -h 128 -d 5 --epochs 1000 -o 'ixr3k_zeta_ca_t750_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
-python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t250.csv' -h 128 -d 5 --epochs 1000 -o 'ixr3k_zeta_ca_t250_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
-python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t10.csv' -h 128 -d 5 --epochs 1000 -o 'ixr3k_zeta_ca_t10_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
-python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t1750.csv' -h 128 -d 5 --epochs 1000 -o 'ixr3k_zeta_ca_t1750_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
+python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t750.csv' -h 128 -d 6 --epochs 1000 -o 'ixr3k_zeta_ca_t750_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
+python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t250.csv' -h 128 -d 6 --epochs 1000 -o 'ixr3k_zeta_ca_t250_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
+python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t10.csv' -h 128 -d 6 --epochs 1000 -o 'ixr3k_zeta_ca_t10_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
+python3 main.py -i 'data/static/indrani/indrani_zeta_ca_t1750.csv' -h 128 -d 6 --epochs 1000 -o 'ixr3k_zeta_ca_t1750_res_batch' --save --gpu --normalize --normalize_out --type res_nn --batch 500
 
 git add .
 git commit -m "$"
