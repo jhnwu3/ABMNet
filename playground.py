5bb4ab149f19cdf87247741ab8ea11c5b8bf3d90
John Wu <jhnwu3@gmail.com>
2023-06-30 12:30:00 -0400

    $

--- playground.py
+++ playground.py
@@ -10,3 +10,9 @@ print(len(combined["outputs"]))
 print(len(combined["rates"]))
 
 seq_data = TemporalDataset(path,min_max_scale=False, standardize_inputs=False)
+# find 4 time points
+time_points = [10,250,750,1750]
+# process these into a new dataset, and then train
+for t in time_points:
+    data = generate_static_dataset(seq_data,t)
+    data.write_to_csv("data/static/indrani/indrani_zeta_ca_t" +str(t) + ".csv")
