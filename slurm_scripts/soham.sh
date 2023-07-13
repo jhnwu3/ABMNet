dceccbeddca8d7df4d3af522bbdaf01062ca0e3a
John Wu <jhnwu3@gmail.com>
2023-07-13 17:30:22 -0400

    $

--- slurm_scripts/soham.sh
+++ slurm_scripts/soham.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=0-99:10:00 
-#SBATCH --job-name=abmnet_indrani
+#SBATCH --job-name=abmnet_soham
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --output=./data/slurm_outputs/indrani_zeta_ca%j.txt
+#SBATCH --output=./data/slurm_outputs/soham%j.txt
 #SBATCH --cpus-per-task=5
 #SBATCH --gres=gpu
 
