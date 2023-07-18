7b069e19501d5d09696a21725fcb28ab2c63478d
John Wu <jhnwu3@gmail.com>
2023-07-18 18:40:13 -0400

    $

--- temporal_transformer.py
+++ temporal_transformer.py
@@ -5,10 +5,10 @@ from modules.utils.evaluate import *
 from sklearn.model_selection import KFold
 import torch
 
-dataset = TemporalDataset("data/time_series/indrani_zeta_ca_h_no_zeroes.pickle", 
-                                   standardize_inputs=True, min_max_scale=True)
+dataset = TemporalDataset("data/time_series/indrani_zeta_ca_h_std_norm.pickle", 
+                                   standardize_inputs=False, min_max_scale=False)
 
-dataset.save_to_pickle("data/time_series/indrani_zeta_ca_h_std_norm.pickle")
+# dataset.save_to_pickle("data/time_series/indrani_zeta_ca_h_std_norm.pickle")
 # output dimension is the same as input dimension (I believe)
 train_temporal_transformer(dataset=dataset, n_rates = dataset.n_rates, hidden_dim=128, 
                            output_dim= dataset.input_size, nEpochs=5)
