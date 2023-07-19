aadbb78151bf7c93cd51cf47f2ded4ed04965765
John Wu <jhnwu3@gmail.com>
2023-07-19 12:42:33 -0400

    $

--- modules/utils/train.py
+++ modules/utils/train.py
@@ -304,8 +304,8 @@ def train_temporal_transformer(dataset, n_rates, hidden_dim, output_dim, nEpochs
             optimizer.zero_grad()
             if batch_size is not None: 
                 rates = rates.unsqueeze(dim=1)
-            print(rates.size())
-            print(in_seq.size())
+            # print(rates.size())
+            # print(in_seq.size())
             prediction = model.forward(rates.to(device), in_seq.to(device))
             loss = criterion(prediction.squeeze(), out_seq.to(device).squeeze())
             loss_this_epoch += loss.item() 
