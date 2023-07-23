4a1ab70ed4caa64fea8a22bf44a3f08539eaf05d
John Wu <jhnwu3@gmail.com>
2023-07-23 14:46:53 -0400

    $

--- temporal_transformer.py
+++ temporal_transformer.py
@@ -16,13 +16,13 @@ train_size = int(0.85 * len(dataset))
 test_size = len(dataset) - train_size
 
 # split dataset, into training set and test
-# train_dataset, test_dataset = tc.utils.data.random_split(dataset, [train_size, test_size])
+train_dataset, test_dataset = tc.utils.data.random_split(dataset, [train_size, test_size])
 
 # output dimension is the same as input dimension (I believe)
-# model = train_temporal_transformer(dataset=train_dataset, n_rates = dataset.n_rates, hidden_dim=128, 
-                        #    output_dim=dataset.input_size, nEpochs=50, batch_size=10)
-# tc.save(model, 'model/indrani_transformer.pt')
-model = tc.load("model/indrani_transformer.pt")
+model = train_temporal_transformer(dataset=train_dataset, n_rates = dataset.n_rates, hidden_dim=128, 
+                           output_dim=dataset.input_size, nEpochs=50, batch_size=10)
+tc.save(model, 'model/indrani_transformer.pt')
+# model = tc.load("model/indrani_transformer.pt")
 criterion = torch.nn.MSELoss() 
 device = tc.device("cpu")
 if tc.cuda.is_available():
@@ -31,19 +31,20 @@ if tc.cuda.is_available():
     criterion = criterion.cuda()
     using_gpu = True
 
-# avg_test_mse, truth, predictions, runtime = evaluate_temporal_transformer(dataset, model, criterion, device, batch_size=20)
-# print("Finished Evaluating "+ str(len(dataset)) + " in time (s):" + str(runtime))
-# print("With AVG MSE:", avg_test_mse)
+model.eval()
+avg_test_mse, truth, predictions, runtime = evaluate_temporal_transformer(dataset, model, criterion, device, batch_size=20)
+print("Finished Evaluating "+ str(len(dataset)) + " in time (s):" + str(runtime))
+print("With AVG MSE:", avg_test_mse)
 
-# plot_scatter(truth, predictions, output="transformer_full_dataset")
+plot_scatter(truth, predictions, output="transformer")
 
 # evaluate with indrani's estimates, and observed dataset's trajectories. 
 # let's see what we get out. 
-
+# THIS SHOULD FAIL D:
 # get indrani's estimates 
 data_CD3z_46L_50k = np.loadtxt("data/John_Indrani_data/zeta_Ca_signal/test_data_experiments/CD3z_46L_50k.dat")
 # convert trajectory into tensor
-model.eval()
+# model.eval()
 indrani_estimates = TemporalDataset("data/time_series/ixr_est_copies.pickle", min_max_scale=True, standardize_inputs=True)
 # torch.utils.data.DataLoader(indrani_estimates, batch_size=5, shuffle=False, drop_last=False)
 fig, axes = plt.subplots(6, 5, figsize=(15, 15))
@@ -63,10 +64,12 @@ plt.savefig("Transformer_validation_ixr_est.png")
 # other sanity check, run same parameter sets, different trajectories
 firstTrajectory = dataset.outputs[0]
 pseudoRates = torch.zeros(1,5).double()
-pseudoRates -= torch.ones(1,5).double()
+# pseudoRates -= torch.ones(1,5).double()
 prediction = model(pseudoRates.to(device), firstTrajectory.to(device))
 plt.figure()
 plt.plot(prediction.detach().cpu().numpy(), c='orange', label="Prediction With Zero")
 plt.plot(firstTrajectory.detach().cpu().numpy(), c='blue', label="Ground Truth")
 plt.savefig("TransformerSanityCheck.png")
+
+
 # perform parameter estimation, this should fail if the rates have no value. 
