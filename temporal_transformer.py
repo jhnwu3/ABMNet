6b405a00f93fab81925de0df06e00cd3008e1b1a
John Wu <jhnwu3@gmail.com>
2023-07-23 14:35:06 -0400

    $

--- temporal_transformer.py
+++ temporal_transformer.py
@@ -43,6 +43,7 @@ if tc.cuda.is_available():
 # get indrani's estimates 
 data_CD3z_46L_50k = np.loadtxt("data/John_Indrani_data/zeta_Ca_signal/test_data_experiments/CD3z_46L_50k.dat")
 # convert trajectory into tensor
+model.eval()
 indrani_estimates = TemporalDataset("data/time_series/ixr_est_copies.pickle", min_max_scale=True, standardize_inputs=True)
 # torch.utils.data.DataLoader(indrani_estimates, batch_size=5, shuffle=False, drop_last=False)
 fig, axes = plt.subplots(6, 5, figsize=(15, 15))
@@ -64,7 +65,7 @@ firstTrajectory = dataset.outputs[0]
 pseudoRates = torch.zeros(1,5)
 prediction = model(pseudoRates.to(device), firstTrajectory.to(device))
 plt.figure()
-plt.plot(prediction, c='orange', label="Prediction With Zero")
-plt.plot(firstTrajectory, c='blue', label="Ground Truth")
+plt.plot(prediction.detach().numpy(), c='orange', label="Prediction With Zero")
+plt.plot(firstTrajectory.detach().numpy(), c='blue', label="Ground Truth")
 plt.savefig("TransformerSanityCheck.png")
 # perform parameter estimation, this should fail if the rates have no value. 
