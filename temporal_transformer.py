ae20ea9602eee5831526f073293389da2551e1f5
John Wu <jhnwu3@gmail.com>
2023-07-25 08:55:40 -0400

    $

--- temporal_transformer.py
+++ temporal_transformer.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import KFold
 import torch
 
 
-fs = 800
+fs = 950
 dataset = TemporalDataset("data/time_series/indrani_zeta_ca_h_std_norm.pickle", 
                                    standardize_inputs=False, min_max_scale=False, steps=fs)
 
@@ -21,10 +21,11 @@ test_size = len(dataset) - train_size
 train_dataset, test_dataset = tc.utils.data.random_split(dataset, [train_size, test_size])
 
 # output dimension is the same as input dimension (I believe)
-# model = train_temporal_transformer(dataset=train_dataset, n_rates = dataset.n_rates, hidden_dim=128, 
-                        #    output_dim=dataset.input_size, nEpochs=50, batch_size=10)
-# tc.save(model, 'model/indrani_transformer_' + str(int(fs)) + '.pt')
-model = tc.load("model/indrani_transformer_" +str(int(fs)) + ".pt")
+model = train_temporal_transformer(dataset=train_dataset, n_rates = dataset.n_rates, hidden_dim=128, 
+                           output_dim=dataset.input_size, nEpochs=50, batch_size=10)
+tc.save(model, 'model/indrani_transformer_' + str(int(fs)) + '.pt')
+# model = tc.load("model/indrani_transformer_" +str(int(fs)) + ".pt")
+
 criterion = torch.nn.MSELoss() 
 device = tc.device("cpu")
 if tc.cuda.is_available():
@@ -45,7 +46,6 @@ plot_scatter(truth, predictions, output="transformer" + str(fs))
 # get indrani's estimates 
 data_CD3z_46L_50k = np.loadtxt("data/John_Indrani_data/zeta_Ca_signal/test_data_experiments/CD3z_46L_50k.dat")
 # convert trajectory into tensor
-# model.eval()
 indrani_estimates = TemporalDataset("data/time_series/ixr_est_copies.pickle", min_max_scale=True, standardize_inputs=True, steps=fs)
 # torch.utils.data.DataLoader(indrani_estimates, batch_size=5, shuffle=False, drop_last=False)
 fig, axes = plt.subplots(6, 5, figsize=(15, 15))
@@ -55,9 +55,10 @@ for r in range(6):
         rates, inputs, outputs = indrani_estimates[i]
         rates = rates.unsqueeze(dim=1).t()
         predictions = model(rates.to(device), inputs.to(device))
-        axes[r,c].plot(indrani_estimates.times[fs:], predictions.cpu().detach().numpy(), c='orange')
-        axes[r,c].plot(indrani_estimates.times[fs:], outputs.cpu().detach().numpy(), c='blue')
+        axes[r,c].plot(indrani_estimates.times[fs:], predictions.cpu().detach().numpy(), c='orange', label="predictions")
+        axes[r,c].plot(indrani_estimates.times[fs:], outputs.cpu().detach().numpy(), c='blue', label="truth")
         i+=1
+plt.legend()
 plt.savefig("transformer_validation_ixr_est_"+ str(fs) + ".png")
 
 # good sanity check, run random wildly different parameter sets with the same set of trajectories, what do we get? Ideally, should be a different output of parameter sets.
@@ -70,6 +71,7 @@ prediction = model(pseudoRates.to(device), firstTrajectory.to(device))
 plt.figure()
 plt.plot(prediction.detach().cpu().numpy(), c='orange', label="Prediction With Zero")
 plt.plot(firstTrajectory.detach().cpu().numpy(), c='blue', label="Ground Truth")
+plt.legend()
 plt.savefig("TransformerSanityCheck" +str(fs) + ".png")
 
 
